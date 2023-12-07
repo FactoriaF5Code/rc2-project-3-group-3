6c41edd9d33124c7208efd161b6f1be5d4cdd793
JOYJOS <enjoyjos@hotmail.com>
2023-12-07 11:44:03 +0100

    keys added

--- main.js
+++ main.js
@@ -6,32 +6,59 @@ let sonidoCuerda = new Audio('./sounds/guitarra.mp3');
 let sonidoPercusion = new Audio('./sounds/bateria.mp3');
 let sonidoViento = new Audio('./sounds/trompeta.mp3');
 
+function tiempoReproduccion() {
+  setTimeout(function () {
+    sonidoCuerda.pause();
+    sonidoPercusion.pause();
+    sonidoViento.pause();
+    sonidoCuerda.currentTime = 0; // Reinicia la reproducción al principio
+    sonidoPercusion.currentTime = 0;
+    sonidoViento.currentTime = 0;
+  }, 5000);
+}
+
 cuerda.onclick = function () {
   sonidoCuerda.loop = false;
   sonidoCuerda.play();
 
-  setTimeout(function () {
-    sonidoCuerda.pause();
-    sonidoCuerda.currentTime = 0; // Reinicia la reproducción al principio
-  }, 5000);
+  tiempoReproduccion();
 };
 
 percusion.onclick = function () {
   sonidoPercusion.loop = false;
   sonidoPercusion.play();
 
-  setTimeout(function () {
-    sonidoPercusion.pause();
-    sonidoPercusion.currentTime = 0; // Reinicia la reproducción al principio
-  }, 5000);
+  tiempoReproduccion();
 };
 
 viento.onclick = function () {
   sonidoViento.loop = false;
   sonidoViento.play();
 
-  setTimeout(function () {
-    sonidoViento.pause();
-    sonidoViento.currentTime = 0; // Reinicia la reproducción al principio
-  }, 5000);
+  tiempoReproduccion();
 };
+
+function reproducirConTeclaC(tecla) {
+  if (tecla.key === 'C' || tecla.key === 'c') {
+    sonidoCuerda.play();
+    tiempoReproduccion();
+  }
+}
+
+function reproducirConTeclaP(tecla) {
+  if (tecla.key === 'P' || tecla.key === 'p') {
+    sonidoPercusion.play();
+    tiempoReproduccion();
+  }
+}
+
+function reproducirConTeclaC(tecla) {
+  if (tecla.key === 'V' || tecla.key === 'v') {
+    sonidoViento.play();
+    tiempoReproduccion();
+  }
+}
+
+document.addEventListener('keydown', reproducirConTeclaC);
+document.addEventListener('keydown', reproducirConTeclaP);
+document.addEventListener('keydown', reproducirConTeclaV);
